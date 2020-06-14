5ffc55b795e5bb17d3a1066cd177ee096e1f3730
Will Ross <paxswill@paxswill.com>
2020-06-14 07:45:01 -0400

    Add an abstract class for FanController types

---
+++ FanController.h
@@ -0,0 +1,47 @@
+#ifndef FAN_FAN_CONTROLLER_H
+#define FAN_FAN_CONTROLLER_H
+
+#include <float.h>
+#include "Arduino.h"
+#include "Fan.h"
+
+class FanController {
+  public:
+    FanController(Fan *fan, float minValue = FLT_MIN, float maxValue = FLT_MAX):
+      minValue(minValue),
+      maxValue(maxValue),
+      fan(fan)
+    {};
+
+    // A short display name describing this controller type.
+    const char * name;
+
+    // The abbreviation for the units for the set point.
+    static const char * valueUnits;
+
+    // A suggested amount to increment the set point value by.
+    const float valueStep = 0;
+
+    // The minimum value for the set point.
+    const float minValue;
+
+    // The maximum value for the set point.
+    const float maxValue;
+
+    // Get the current set point.
+    virtual float getValue() = 0;
+
+    // Set a new set point.
+    virtual void setValue(float newValue) = 0;
+
+    /* Convenience function that calls `periodic()` with the result of
+     * `millis()`
+     */
+    void periodic() { periodic(millis()); };
+
+    // Called each iteration of the run loop.
+    virtual void periodic(unsigned long currentMillis) = 0;
+  protected:
+    Fan *fan;
+};
+#endif
