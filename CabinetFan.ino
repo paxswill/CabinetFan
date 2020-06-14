43d9e060160a18d69cb7a8761ec027cf14a124cd
Will Ross <paxswill@paxswill.com>
2020-06-14 09:38:44 -0400

    Add groundwork for selectable control schemes
    Right now just an open-loop, constant speed control scheme is
    implemented. Eventually I plan to implement closed-loop P and PI schemes
    As well.

--- CabinetFan.ino
+++ CabinetFan.ino
@@ -4,6 +4,8 @@
 #include "Thermometer.h"
 #include "Fan.h"
 #include "util.h"
+#include "FanController.h"
+#include "ConstantSpeed.h"
 
 // Pin Definitions are for the 32u4 Adafruit ItsyBitsy
 const byte tachPin = 0;     // Digital 0   PD2  INT2
@@ -13,6 +15,8 @@ const byte tempPin = A11;    // D12/A11     PD6  ADC9
 Thermometer externalThermo = Thermometer(tempPin);
 Thermometer internalThermo = Thermometer();
 Fan *fan;
+FanController *currentController;
+ConstantSpeedController *speedController;
 
 void setup() {
   /* Fan needs to be constructed *after* Arduino setup has completed, or else
@@ -24,6 +28,12 @@ void setup() {
   */
   fan = new Fan(controlPin, Fan::NOT_SET, 1200, 5000, phaseFrequencyCorrect);
   fan->setSpeed(0.8);
+  /* Create all fan controllers ahead of time, and then set `currentController`
+   * as needed.
+   */
+  speedController = new ConstantSpeedController(fan);
+  // Right now there's only one controller implemented.
+  currentController = speedController;
 }
 
 // Implicitly set to 0
@@ -40,4 +50,5 @@ void loop() {
     lastUpdate = currentMillis;
   }
   fan->periodic(currentMillis);
+  currentController->periodic(currentMillis);
 }
