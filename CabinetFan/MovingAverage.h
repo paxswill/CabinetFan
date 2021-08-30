621b71e83417f2853d2f539ae6f413d7332cbbaa
Will Ross <paxswill@paxswill.com>
2021-08-30 12:42:01 -0400

    Rewrite MovingAverage to use a circular array
    Lower memory usage than the linked list version, and no dynamic memory
    allocation.

--- CabinetFan/MovingAverage.h
+++ CabinetFan/MovingAverage.h
@@ -1,45 +1,42 @@
 #ifndef FAN_MOVING_AVERAGE_H
 #define FAN_MOVING_AVERAGE_H
 
-#include "Arduino.h"
-
 template<typename T = float, uint8_t N = 10>
 class MovingAverage {
   public:
     /* Create a `MovingAverage` that averages the last N values.
     */
     MovingAverage():
-      head(NULL),
-      tail(NULL),
-      current_length(0),
-      sum(0)
-    {};
+      count(0),
+      sum(0),
+      values {0}
+    {
+      current = values;
+    };
 
     /* Add a new value to the moving average.
      */
     void push(T new_value) {
-      if (current_length >= N) {
-        // Subtract and delete the old node
-        Node * old_tail = tail;
-        sum -= old_tail->value();
-        tail = old_tail->pop();
-        current_length -= 1;
-      }
-      // Push a new head node, and update the sum
-      if (head == NULL) {
-        head = new Node(new_value);
-        tail = head;
+      if (count == N) {
+        // Need to subtract the value being replaced first
+        sum -= *current;
       } else {
-        head = head->push(new_value);
+        // Keep track of the number of elements as we're filling the list.
+        count++;
       }
-      sum += head->value();
-      current_length +=1;
+      *current = new_value;
+      sum += new_value;
+      // Increment `current`
+      current++;
+      if (current >= (values + N)) {
+        current = values;
+      }
     }
 
     /* Calculate the current moving average.
      */
     T current_value() {
-      return sum / T(current_length);
+      return sum / T(count);
     }
 
     /* Add a new value and calculate the current movign average.
@@ -50,69 +47,23 @@ class MovingAverage {
     }
 
   private:
-    class Node {
-      public:
-        Node(T value): _value(value), _next(NULL), _prev(NULL) {};
+    // The current number of values in the average.
+    uint8_t count;
 
-        ~Node() {
-          if (_next != NULL) {
-            _next->_prev = _prev;
-          }
-          if (_prev != NULL) {
-            _prev->_next = _next;
-          }
-        }
-
-        // Prepend a new node to this linked list, returning the new head.
-        Node * push(Node * new_node) {
-          Node * old_head = this;
-          while (old_head->_prev != NULL) {
-            old_head = old_head->_prev;
-          }
-          new_node->_next = old_head;
-          old_head->_prev = new_node;
-          return new_node;
-        }
-
-        // Prepend a new value to this linked list, returning the new head.
-        Node * push(T new_value) {
-          Node * new_node = new Node(new_value);
-          return this->push(new_node);
-        }
-
-        // Delete the last node of this linked list, returning the new tail.
-        Node * pop() {
-          Node * old_tail = this;
-          while (old_tail->_next != NULL) {
-            old_tail = old_tail->_next;
-          }
-          Node * new_tail = old_tail->_prev;
-          delete old_tail;
-          return new_tail;
-        }
-
-        // Retrieve value of this node.
-        T value() {
-          return this->_value;
-        }
-
-        Node * next() {
-          return this->_next;
-        }
-
-        Node * prev() {
-          return this->_prev;
-        }
-      private:
-        T _value;
-        Node * _next;
-        Node * _prev;
-    };
-
-    Node *head;
-    Node *tail;
-    uint8_t current_length;
+    /* The sum of all values.
+     * This is an optimization so that adding only happens on adding an object
+     * to the list, instead of every value being summed every time the average
+     * is being retrieved.
+     */
     T sum;
+
+    /* The values that are being averaged.
+     * This is a circular buffer, accessed through the `current` pointer.
+     */
+    T values[N];
+
+    // The address of the next value to be added to the buffer.
+    T * current;
 };
 
 #endif
